b77a2e0514747b9111b0ffc0774f0b4d01947358
gilgil <gilgil@gilgil.net>
2026-03-23 03:07:34 +0900

    Add sudemon.rc

--- bin/android-install.bat
+++ bin/android-install.bat
@@ -4,7 +4,9 @@ adb push su /system/bin/su2
 adb exec-out "mv /system/bin/su2 /system/bin/su"
 adb push sudemon /system/bin/sudemon
 adb push sudemon.sh /system/bin/sudemon.sh
+adb push sudemon.rc /system/etc/init/sudemon.rc
 adb exec-out "chmod 755 /system/bin/su'"
 adb exec-out "chmod 755 /system/bin/sudemon"
 adb exec-out "chmod 755 /system/bin/sudemon.sh"
+adb exec-out "chmod 644 /system/etc/init/sudemon.rc"
 call .\android-start.bat
--- bin/android-uninstall.bat
+++ bin/android-uninstall.bat
@@ -2,4 +2,4 @@ call .\android-stop.bat
 adb root
 adb exec-out "pkill sudemon"
 adb remount
-adb exec-out "rm -f /system/bin/su /system/bin/sudemon /system/bin/sudemon.sh"
+adb exec-out "rm -f /system/bin/su /system/bin/sudemon /system/bin/sudemon.sh /system/etc/init/sudemon.rc"
